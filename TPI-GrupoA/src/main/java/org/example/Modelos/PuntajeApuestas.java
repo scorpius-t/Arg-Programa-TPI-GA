9dd75bbc699e5fe99d589b4e29bbce4abf90d3e2
scorpius-t <scorpius_t@hotmail.com>
2023-03-28 10:50:19 -0300

    Calculo de puntaje OK2 - Falta corregir ArrayList no usados y reestructurar funciones

--- TPI-GrupoA/src/main/java/org/example/Modelos/PuntajeApuestas.java
+++ TPI-GrupoA/src/main/java/org/example/Modelos/PuntajeApuestas.java
@@ -5,48 +5,20 @@ import java.util.List;
 import java.util.Map;
 
 public class PuntajeApuestas {
-    private Map<String,Integer> apostadorPuntos=new HashMap<>();
-    private String apostador2;
-    private Integer puntos2;
+    private Map<String, Integer> apostadorPuntos = new HashMap<>();
 
-    public String getApostador2() {
-        return apostador2;
+    public Map<String, Integer> getApostadorPuntos() {
+        return apostadorPuntos;
     }
 
-    public void setApostador2(String apostador2) {
-        this.apostador2 = apostador2;
+    public void setApostadorPuntos(Map<String, Integer> apostadorPuntos) {
+        this.apostadorPuntos = apostadorPuntos;
     }
 
-    public Integer getPuntos2() {
-        return puntos2;
-    }
+    public Map<String, Integer> calculoDePuntos(List<Ronda> rondas, List<Pronostico> pronosticos, int ptoPartido, int ptoGoles, int ptoRonda) {
+        Map<String, Integer> puntaje = new HashMap<>();
 
-    public void setPuntos2(Integer puntos2) {
-        this.puntos2 = puntos2;
-    }
-/*
-    public PuntajeApuestas(String apostador, Integer puntos) {
-        this.setApostador2(apostador);
-        this.setPuntos2(puntos);
-    }
-*/
-    public Map<String,Integer> calculoDePuntos(List<Ronda> rondas, List<Pronostico> pronosticos , int ptoPartido, int ptoGoles, int ptoRonda){
-        Map<String,Integer> puntaje=new HashMap<>();
-
-
-        /*
-        Calcular puntaje
-
-
-        recorrer partidos de todas las rondas comparando equiposL y equivosV de esta clase. Si hay coincidencia,
-        comparar goles de pronostico con ronda (si V>L V=L V<L). Si hay acierto, ver en puntaje (lista de apostadores y puntaje)
-        si existe apostador (si no existe crearlo), sumar ptoPartido a puntaje.puntaje (usando getter y setter)
-
-        devolver puntaje
-
-         */
-
-        for(Pronostico pronostico:pronosticos) {
+        for (Pronostico pronostico : pronosticos) {
             if (puntaje.isEmpty())
                 puntaje.put(pronostico.getApostador(), 0);
 
@@ -55,55 +27,58 @@ public class PuntajeApuestas {
                 puntaje.put(pronostico.getApostador(), 0);
 
 
-            Partido partidoPronostico=new Partido();
-            //for(Partido partidoPronostico:pronostico.getPartidos()){
-            if (pronostico.getPartidos().size()==1) {
+            Partido partidoPronostico = new Partido();
+
+            if (pronostico.getPartidos().size() == 1) {
                 partidoPronostico = pronostico.getPartidos().get(pronostico.getPartidos().size() - 1);
             } else {
                 System.out.println("ERROR INDICE LINEA PRONOSTICO");
                 puntaje.clear();
-                puntaje.put("ERROR CALCULO",0);
+                puntaje.put("ERROR CALCULO", 0);
+                this.setApostadorPuntos(puntaje);
                 return puntaje;
             }
 
-                for (Ronda ronda : rondas) {
+            for (Ronda ronda : rondas) {
 
-                    List<Partido> partidoDeRonda = ronda.getPartidos();
+                List<Partido> partidoDeRonda = ronda.getPartidos();
 
-                    for (Partido partido : partidoDeRonda) {
+
+                for (Partido partido : partidoDeRonda) {
 /*
                         String debug1=partido.getEquipoL().getNombre()+partido.getEquipoV().getNombre();
                         String debug2=partidoPronostico.getEquipoL().getNombre()+partidoPronostico.getEquipoV().getNombre();
                         System.out.println(debug1+"\n"+debug2);
 */
-                        if(partido.getEquipoL().getNombre().equalsIgnoreCase(partidoPronostico.getEquipoL().getNombre())&&
-                                partido.getEquipoV().getNombre().equalsIgnoreCase(partidoPronostico.getEquipoV().getNombre())){
-                            boolean isPronosticoLocal= (partido.getGolesEquipoL()>partido.getGetGolesEquipoV() && partidoPronostico.getGolesEquipoL()>partidoPronostico.getGetGolesEquipoV());
-                            boolean isPronosticoVisitante= (partido.getGolesEquipoL()<partido.getGetGolesEquipoV() && partidoPronostico.getGolesEquipoL()<partidoPronostico.getGetGolesEquipoV());
-                            boolean isPronosticoEmpate= (partido.getGolesEquipoL()==partido.getGetGolesEquipoV() && partidoPronostico.getGolesEquipoL()==partidoPronostico.getGetGolesEquipoV());
+                    if (partido.getEquipoL().getNombre().equalsIgnoreCase(partidoPronostico.getEquipoL().getNombre()) &&
+                            partido.getEquipoV().getNombre().equalsIgnoreCase(partidoPronostico.getEquipoV().getNombre())) {
+                        boolean isPronosticoLocal = (partido.getGolesEquipoL() > partido.getGetGolesEquipoV() && partidoPronostico.getGolesEquipoL() > partidoPronostico.getGetGolesEquipoV());
+                        boolean isPronosticoVisitante = (partido.getGolesEquipoL() < partido.getGetGolesEquipoV() && partidoPronostico.getGolesEquipoL() < partidoPronostico.getGetGolesEquipoV());
+                        boolean isPronosticoEmpate = (partido.getGolesEquipoL() == partido.getGetGolesEquipoV() && partidoPronostico.getGolesEquipoL() == partidoPronostico.getGetGolesEquipoV());
 
-                            if(isPronosticoLocal||isPronosticoEmpate||isPronosticoVisitante) {
+                        if (isPronosticoLocal || isPronosticoEmpate || isPronosticoVisitante) {
 
-                                Integer puntos = puntaje.get(pronostico.getApostador()) + ptoPartido;
-                                puntaje.put(pronostico.getApostador(), puntos);
-                               // System.out.println(pronostico.getApostador() + puntaje.get(pronostico.getApostador())+"\n---------------------------");
+                            Integer puntos = puntaje.get(pronostico.getApostador()) + ptoPartido;
+                            puntaje.put(pronostico.getApostador(), puntos);
+                            // System.out.println(pronostico.getApostador() + puntaje.get(pronostico.getApostador())+"\n---------------------------");
 
-                            }
                         }
 
+
                     }
+
+
                 }
-           // }
+
+            }
+
 
         }
-
+        this.setApostadorPuntos(puntaje);
         return puntaje;
     }
-    @Override
+
     public String toString() {
-        return "PuntajeApuestas{" +
-                "apostador='" + apostador2 + '\'' +
-                ", puntos=" + puntos2 +
-                '}';
+        return apostadorPuntos.toString();
     }
 }
